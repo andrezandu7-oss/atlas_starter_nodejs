83427e4eb724b483951f9236da6f3ddc1059f817
andrezandu7-oss <andrezandu7@gmail.com>
2026-02-16 12:47:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const webPush = require('web-push');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static('public'));
 
+// Connexion MongoDB
 const mongoURI = "mongodb+srv://Genlove:le09022025rose%40@cluster0.6vdjyyo.mongodb.net/?appName=Cluster0";
 
 mongoose.connect(mongoURI)
   .then(() => console.log("✅ MongoDB connecté"))
   .catch(err => console.error("❌ Erreur MongoDB:", err.message));
 
+// Route de test
 app.get('/', (req, res) => {
-  res.send("Genlove API en ligne");
+  res.send("🚀 Genlove API en ligne");
 });
 
 const PORT = process.env.PORT || 3000;
